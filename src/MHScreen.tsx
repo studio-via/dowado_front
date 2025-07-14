8eaeea4d398c502d47f912ad9ef4647ae998f022
Baetato <diana021015@gmail.com>
2025-07-15 02:01:37 +0900

    feat: MH

--- src/MHScreen.tsx
+++ src/MHScreen.tsx
@@ -1,14 +1,38 @@
-export default function App() {
+import React from 'react';
+import styled from 'styled-components';
+import FixedScheduleBox from './FixedScheduleBox';
+import GenerateProjectBox from './GenerateProjectBox';
+import Carousel from './Carousel';
+import SessionList from './SessionList';
+
+const Container = styled.div`
+  width: 402px;
+  height: 874px;
+  display: flex;
+  flex-direction: column;
+  padding: 16px;
+  box-sizing: border-box;
+  overflow-y: auto;
+`;
+
+const Title = styled.h1`
+  font-size: 1.25rem;
+  font-weight: 600;
+  line-height: 1.4;
+  margin-bottom: 16px;
+  white-space: pre-line;
+`;
+
+const MHScreen: React.FC = () => {
   return (
-    <div
-      style={{
-        width: '402px',
-        height: '827px',
-        backgroundColor: '#f0f0f0',
-        margin: '0 auto'
-      }}
-    >
-      <h1>iOS 앱 기준 1453×205 영역</h1>
-    </div>
+    <Container>
+      <Title>아아아 배수연님!\n오늘도 미루시나요?</Title>
+      <FixedScheduleBox />
+      <GenerateProjectBox />
+      <Carousel />
+      <SessionList />
+    </Container>
   );
-}
+};
+
+export default MHScreen;
