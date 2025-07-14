d50c5c8ddccec73f0744bfb8a7d0f2377de10047
Baetato <diana021015@gmail.com>
2025-07-15 02:01:04 +0900

    initial setting

---
+++ src/MHScreen.tsx
@@ -0,0 +1,14 @@
+export default function App() {
+  return (
+    <div
+      style={{
+        width: '402px',
+        height: '827px',
+        backgroundColor: '#f0f0f0',
+        margin: '0 auto'
+      }}
+    >
+      <h1>iOS 앱 기준 1453×205 영역</h1>
+    </div>
+  );
+}
